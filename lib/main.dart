b513fa494967f6a13c2a727e43dc5afa105d6a8e
Emad <36300278+emadsaeed3@users.noreply.github.com>
2021-05-12 07:58:46 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:pharma/components/authservice.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'constants.dart';
 
 
