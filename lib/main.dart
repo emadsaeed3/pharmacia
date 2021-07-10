3e89444f23ef52607987ef47a202eb64a0061582
Emad <36300278+emadsaeed3@users.noreply.github.com>
2021-07-11 00:54:37 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,50 +2,23 @@ import 'dart:async';
 import 'package:pharma/components/authservice.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'package:pharma/components/dark_theme_provider.dart';
-import 'package:pharma/components/theme_data.dart';
-
-
+import 'package:pharma/components/shared_prefrances.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  initpref();
   runApp(MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
-  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
-
-  void getCurrentAppTheme() async {
-    themeChangeProvider.darkTheme =
-    await themeChangeProvider.darkThemePreferences.getTheme();
-  }
-
-
-
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-        ChangeNotifierProvider(create: (_) {
-      return themeChangeProvider;
-    })
-    ],
-    child:
-    Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pharmacia',
-      home:SplashScreen(),
-      theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-    );
-    }));
-  }
-}
+            debugShowCheckedModeBanner: false,
+            title: 'Pharmacia',
+            home: SplashScreen(),);}}
 
 
 class SplashScreen extends StatefulWidget {
@@ -53,17 +26,16 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-
-
-
 class _SplashScreenState extends State<SplashScreen> {
+  String role = 'user';
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () =>  Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (BuildContext context) =>  AuthService().handleAuth())));
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => AuthService().handleAuth())));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -73,39 +45,23 @@ class _SplashScreenState extends State<SplashScreen> {
         fit: StackFit.expand,
         children: <Widget>[
           Container(
-            decoration: BoxDecoration(image: DecorationImage(
-                image: AssetImage("images/wallpaper.png"),
-                fit: BoxFit.cover
-            )),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/wallpaper.png"),
+                    fit: BoxFit.cover)),),
+          Column(mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Expanded(
-                flex: 2,
+              Expanded(flex: 2,
                 child: Container(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Image.asset('images/pharmacia_logo.png', height: size.height * 0.30  ),
-                      SizedBox(height: size.height * 0.01),
-                    ],
-                  ),
-                ),
-              ),
+                      Image.asset('images/pharmacia_logo.png',
+                          height: size.height * 0.30),
+                      SizedBox(height: size.height * 0.01),],),),),
               Expanded(
                 flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    CircularProgressIndicator(),
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+                    CircularProgressIndicator(),],),)],)],),);}}
